5828e2121e43be20865a80b2a1033b1b8870646f
Dickson Lee <dicksonT_lee@hotmail.com>
2025-02-24 17:55:27 -0500

    fix issue

--- week/1_FileProcessProject/src/fileProcess/TestFileProcess1.java
+++ week/1_FileProcessProject/src/fileProcess/TestFileProcess1.java
@@ -1,12 +1,17 @@
+// Compile:
+// On ~/420-930-va/week/1_FileProcessProject/src
+// javac fileProcess/TestFileProcess1.java
+// java fileProcess/TestFileProcess1.java
+
 package fileProcess;
 
 import java.util.*;
 import java.io.*;
 
 public class TestFileProcess1 {
-  public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws IOException {
     System.out.println("hello world");
-    Scanner inFile = new Scanner(new FileReader("../../data.in"));
+    Scanner inFile = new Scanner(new FileReader("../data.in"));
 
     double x, y;
     while (inFile.hasNextLine()) {
